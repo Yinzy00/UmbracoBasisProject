0c4fc9b2d6c06cc127d6f801e0e808e8880c0322
Yinzy00 <yari.marien@telenet.be>
2022-03-10 11:32:36 +0100

    created meta tags property editor

---
+++ App_Plugins/MyMetaTags/js/MetaTagsPluginController.js
@@ -0,0 +1,5 @@
+﻿angular.module('umbraco').controller('MetaTagsPluginController', function ($scope) {
+    if ($scope.model.value === null || $scope.model.value === "") {
+        $scope.model.value = {noFollow:false, noIndex:false};
+    }
+});
